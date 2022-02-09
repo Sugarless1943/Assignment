97bd85527a6431361486ed7a230d3d7f07368937
yefan <13259459957@163.com>
2022-02-09 17:01:31 +0800

    'init'

---
+++ src/components/HomeNav.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="home-nav">
+      <header>
+        Assignment
+      </header>
+      <main>
+        <ul>
+          <li>part1</li>
+          <li>part2</li>
+        </ul>
+      </main>
+    </div>
+   
+</template>
+
+<script>
+export default {
+    name: "Nav",
+    data() {
+        return {
+          
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  .home-nav {
+    &>header {
+      height: 60px;
+      line-height: 60px;
+      font-weight: bold;
+    }
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-aside width="200px">
+      <home-nav></home-nav>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import homeNav from '../components/HomeNav.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    homeNav
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .el-container {
+    height: 100%;
+  }
+</style>
